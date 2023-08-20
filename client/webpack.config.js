e3db3638665d74fe4f4669facd14e837ebbda0ef
Karelyn Rojas <knrojas@ucdavis.edu>
2023-08-20 15:40:05 -0700

    add plugins and babel,style loader to webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -18,12 +15,53 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      // add html webpack plugin
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        title: 'Html Webpack Plugin',
+      }),
+      // add manifest plugin
+      new WebpackPwaManifest({
+        name: 'Just Another Text Editor PWA',
+        short_name: 'JATE',
+        description: 'A simple offline text editor PWA',
+        background_color: "#225ca3",
+        theme_color: "#225ca3",
+        starturl: "/",
+        publicPath: "/",
+        fingerprints: false,
+        inject: true,
+        icons: [
+          {
+            src: path.resolve('src/assets/icon.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons')
+          }
+        ]
+      }),
+      // add workbox plugin
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
     ],
-
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        }, // add css and style loader
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime']
+            } // add babel loader
+          }
+        },
       ],
     },
   };
